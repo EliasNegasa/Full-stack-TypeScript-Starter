9f0130852c12532f6bba8244ab2fa7ac7d0c4501
Elias.Negasa <eliasnegasa7@gmail.com>
2025-09-19 19:39:41 -0400

    Setup Prettier

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,24 +1,23 @@
-import { useEffect, useState } from "react";
-import { Button } from "./components/ui/button";
-
+import { useEffect, useState } from 'react';
+import { Button } from './components/ui/button';
 
 function App() {
-  const [message, setMessage] = useState("");
+   const [message, setMessage] = useState('');
 
-  useEffect(() => {
-    fetch("/api/hello")
-      .then((response) => response.json())
-      .then((data) => setMessage(data.message))
-      .catch((error) => console.error("Error fetching message:", error));
-  }, []);
+   useEffect(() => {
+      fetch('/api/hello')
+         .then((response) => response.json())
+         .then((data) => setMessage(data.message))
+         .catch((error) => console.error('Error fetching message:', error));
+   }, []);
 
-  return (
-    <div className="p-4">
-      <h1 className="text-3xl text-amber-500">Frontend</h1>
-      <p>{message || "Loading..."}</p>
-      <Button>Click</Button>
-    </div>
-  );
+   return (
+      <div className="p-4">
+         <h1 className="text-3xl text-amber-500">Frontend</h1>
+         <p>{message || 'Loading...'}</p>
+         <Button>Click</Button>
+      </div>
+   );
 }
 
-export default App
+export default App;
